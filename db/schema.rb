72e8955a940ab7c768973d13ba5b0dca9f9bcb93
Dmitry Shlyatsky <42865006+Dmitryshl@users.noreply.github.com>
2022-08-21 22:08:18 +0300

    fix description

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_21_175150) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "about"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
