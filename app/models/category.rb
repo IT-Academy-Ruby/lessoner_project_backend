bf2fde0cbfdb342120742d5124135f6431a6485c
evrusha <uzaharau@gmail.com>
2022-08-23 20:49:36 +0300

    Updated2

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
-    include Visible
+
+    enum :status, [:active, :archived]
 
     validates :name, presence: true
     validates :description, presence: true
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_183428) do
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_172200) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
@@ -19,7 +6,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_183428) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.integer "status", default: 0
   end
 
 end
