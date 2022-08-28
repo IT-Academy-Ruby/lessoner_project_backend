c5c435b212d178248217ff2592828b58a2c3145b
ritamma <rita_meschanskaya@mail.ru>
2022-08-28 20:56:34 +0300

    add-comments-update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :lesson_find, only: [:create, :edit, :update]
 
  def create
-    @lesson = Lesson.find(params[:lesson_id])
-    @comment = @lesson.comments.create!(comment_params.merge(user:current_user))
+    @comment = current_user.comments.create(comment_params.merge(lesson: @lesson)
     if @comment.save
       flash[:success] = "Comment successfully added"
       redirect_to lesson_path(@lesson)
@@ -35,5 +35,9 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:commenter, :body)
+  end
+
+  def lesson_find
+    @lesson = Lesson.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_155600) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_28_171710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_155600) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
     t.text "body"
     t.bigint "lesson_id", null: false
     t.bigint "user_id", null: false
