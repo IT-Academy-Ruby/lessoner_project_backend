e52623e4f68928ee9b6f3cb37f43fdc27e2d85d4
evrusha <uzaharau@gmail.com>
2022-08-31 00:11:42 +0300

    added i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
-  root 'static_pages#home'
-  get '/about', to: 'static_pages#about'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :categories
+    root 'static_pages#home'
+    get '/about', to: 'static_pages#about'
+  end
 end
