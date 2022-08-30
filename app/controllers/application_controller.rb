cd78d8963606c129fac6fcc984c3544aaebd5f9d
Sergey Dmitriev <terapool@gmail.com>
2022-08-31 00:40:39 +0300

    Add header, hero layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
     before_action :set_locale
+    helper_method :current_locale
 
     private
 
     def default_url_options
         {locale: I18n.locale}
+    end
+
+    def current_locale
+        I18n.locale
     end
 
     def set_locale
