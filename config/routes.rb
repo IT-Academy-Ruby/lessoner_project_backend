3dddb0666f632045e2cadf1fbca65c0b7be25f0a
evrusha <uzaharau@gmail.com>
2022-08-31 20:19:37 +0300

    Added default root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: redirect("/#{I18n.default_locale}"), as: :redirected_root
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     resources :categories
     root 'static_pages#home'
