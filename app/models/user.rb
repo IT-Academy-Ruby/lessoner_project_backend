ce7411878e89adb11f5f5046885b905ec64a9d9b
ritamma <rita_meschanskaya@mail.ru>
2022-09-02 19:00:45 +0300

    Solved problems with login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable
 
-  validates :name, :email, presence: true
+  validates :email, presence: true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :categories
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
-  devise_for :users
 
 end
