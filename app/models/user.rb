3415eee53f3104495cff13d887495c1df7c837f3
ritamma <rita_meschanskaya@mail.ru>
2022-09-04 18:31:06 +0300

    Add views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true
+  validates :name, :email, presence: true
 
   has_many :comments
 
