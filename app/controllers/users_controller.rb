acabfb971d3694553e01e030504fa43ef04faf10
ritamma <rita_meschanskaya@mail.ru>
2022-09-09 15:48:44 +0300

    [#11] Add CRUD for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  before_action :user_find, only: %i[show edit update destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :description)
+  end
+
+  def user_find
+    @user = User.find(params[:id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, :email, presence: true
+  before_save { self.email = email.downcase }
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_many :comments
-
 end
