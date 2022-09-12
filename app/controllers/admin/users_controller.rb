ffd29444aed93036165a1078027e95780620963b
ritamma <rita_meschanskaya@mail.ru>
2022-09-12 18:16:52 +0300

    [#11] Add namespace

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,49 +1,51 @@
 # frozen_string_literal: true
 
-class Admin::UsersController < ApplicationController
-  before_action :user_find, only: %i[show edit update destroy]
+module Admin
+  class UsersController < ApplicationController
+    before_action :user_find, only: %i[show edit update destroy]
 
-  def index
-    @users = User.all
-  end
-
-  def show; end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render :new, status: :unprocessable_entity
+    def index
+      @users = User.all
     end
-  end
 
-  def edit; end
+    def show; end
 
-  def update
-    if @user.update(user_params)
-      redirect_to @user
-    else
-      render :edit, status: :unprocessable_entity
+    def new
+      @user = User.new
     end
-  end
 
-  def destroy
-    @user.destroy
-    redirect_to admin_users_path
-  end
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to @user
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
 
-  private
+    def edit; end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :description)
-  end
+    def update
+      if @user.update(user_params)
+        redirect_to admin_users_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
 
-  def user_find
-    @user = User.find(params[:id])
+    def destroy
+      @user.destroy
+      redirect_to admin_users_path
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :description)
+    end
+
+    def user_find
+      @user = User.find(params[:id])
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@
 
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_validation { self.email = email.downcase }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -13,6 +12,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  before_validation { self.email = email.downcase }
   validates :password, presence: true, length: { minimum: 6 }
 
   has_many :comments, dependent: :destroy
