739ee3793d463c1b544ede31ec9d61ad51fc3b60
evrusha <uzaharau@gmail.com>
2022-09-13 22:26:03 +0300

    Fixed oauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,12 +8,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def twitter
   # end
   def google_oauth2
-    user = User.from_omniauth(auth)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
-    if user.present?
+    if @user.present?
       sign_out_all_scopes
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect user, event: :authentication
+      sign_in_and_redirect @user, event: :authentication
     else
       flash[:alert] =
         t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized"
@@ -39,9 +39,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
-  private
-
-  def auth
-    @auth ||= request.env['omniauth.auth']
-  end
 end
