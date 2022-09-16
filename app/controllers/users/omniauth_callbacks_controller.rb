5231eb8d852c870f82b89c332a32090511d449dd
evrusha <uzaharau@gmail.com>
2022-09-16 21:08:54 +0300

    Fixed rubo errors

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,28 +9,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # end
   def google_oauth2
     @user = User.from_omniauth(request.env['omniauth.auth'])
-
     if @user.present?
       sign_out_all_scopes
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect @user, event: :authentication
     else
-      flash[:alert] =
-        t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized"
-        redirect_to new_user_session_path
+      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized"
+      redirect_to new_user_session_path
     end
   end
 
   def facebook
     @user = User.from_omniauth(request.env['omniauth.auth'])
-
     if @user.present?
       sign_out_all_scopes
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Facebook'
       sign_in_and_redirect @user, event: :authentication
     else
-      flash[:alert] =
-      t 'devise.omniauth_callbacks.failure', kind: 'Facebook', reason: "#{auth.info.email} is not authorized"
+      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Facebook', reason: "#{auth.info.email} is not authorized"
       redirect_to new_user_session_path
     end
   end
