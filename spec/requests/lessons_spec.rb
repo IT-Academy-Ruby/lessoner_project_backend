95d130878e59783637ba3e4fc05092ab4537d41c
Dmitry Shlyatsky <42865006+Dmitryshl@users.noreply.github.com>
2022-10-04 02:35:05 +0300

    fix comments

--- spec/requests/lessons_spec.rb
+++ spec/requests/lessons_spec.rb
@@ -1,6 +1,7 @@
 require 'swagger_helper'
 
 RSpec.describe 'lessons', type: :request do
+
   path '(/{locale)}/lessons' do
     # You'll want to customize the parameter types...
     parameter name: 'locale', in: :path, type: :string, description: 'locale'
@@ -10,13 +11,13 @@ RSpec.describe 'lessons', type: :request do
 
       response(200, 'successful') do
         schema type: :array,
-               items: {
-                 type: :object,
-                 properties: {
-                   id: { type: :integer },
-                   title: { type: :string },
-                   description: { type: :string }
-                 }
+                items: {
+                  type: :object,
+                  properties: {
+                    id: {type: :integer},
+                    title: {type: :string },
+                    description: {type: :string}
+                  }
                }
         run_test!
       end
@@ -43,47 +44,7 @@ RSpec.describe 'lessons', type: :request do
     end
   end
 
-  path '(/{locale)}/lessons/new' do
-    # You'll want to customize the parameter types...
-    parameter name: 'locale', in: :path, type: :string, description: 'locale'
 
-    get('new lesson') do
-      response(200, 'successful') do
-        let(:locale) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
-  # path '(/{locale)}/lessons/{id}/edit' do
-  #   # You'll want to customize the parameter types...
-  #   parameter name: 'locale', in: :path, type: :string, description: 'locale'
-  #   parameter name: 'id', in: :path, type: :string, description: 'id'
-
-  #   get('edit lesson') do
-  #     response(200, 'successful') do
-  #       let(:locale) { '123' }
-  #       let(:id) { '123' }
-
-  #       after do |example|
-  #         example.metadata[:response][:content] = {
-  #           'application/json' => {
-  #             example: JSON.parse(response.body, symbolize_names: true)
-  #           }
-  #         }
-  #       end
-  #       run_test!
-  #     end
-  #   end
-  # end
 
   path '(/{locale)}/lessons/{id}' do
     # You'll want to customize the parameter types...
