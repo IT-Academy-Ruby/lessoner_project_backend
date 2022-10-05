1219fd63dc609c118a2a7604f08b2362e9d19f6e
evrusha <uzaharau@gmail.com>
2022-10-05 20:44:26 +0300

    Fixed tabs

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -1,16 +1,16 @@
 class SignUpController < ApplicationController
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            render :create
-        else
-            render json: {errors: @user.errors.full_messages}
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :create
+    else
+      render json: {errors: @user.errors.full_messages}
     end
+  end
 
-    private
+  private
 
-    def user_params
-        params.permit(:email, :name, :password, :gender, :birthday, :phone)
-    end
+  def user_params
+    params.permit(:email, :name, :password, :gender, :birthday, :phone)
+  end
 end
