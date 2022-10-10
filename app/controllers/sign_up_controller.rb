988dddc040a22d3434f6753160b557ad0976a75b
evrusha <uzaharau@gmail.com>
2022-10-10 19:29:47 +0300

    Added response statuses

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -2,9 +2,9 @@ class SignUpController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render :create
+      render :create, status: :created
     else
-      render json: { errors: @user.errors.full_messages }
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
     end
   end
 
