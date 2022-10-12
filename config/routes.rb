efc65a4138d7825142191e6348d0f044e3bb3102
ritamma <rita_meschanskaya@mail.ru>
2022-10-12 22:50:08 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :categories
     root 'static_pages#home'
     get '/about', to: 'static_pages#about'
-    get '/check_email', to: 'users#check_email'
+    get '/check_email', to: 'users#check_email', defaults: { format: :json }
     resources :lessons do
       resources :comments
     end
