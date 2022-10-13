0508835742ea2be7b5048f3931d44ac0ee2017d0
evrusha <uzaharau@gmail.com>
2022-10-13 21:37:24 +0300

    added swagger

---
+++ spec/requests/sign_up_spec.rb
@@ -0,0 +1,47 @@
+require 'swagger_helper'
+
+RSpec.describe 'sign_up', type: :request do
+
+  path '/sign_up' do
+
+    post('create sign_up') do
+      tags 'Sign_up'
+      produces 'application/json'
+      parameter name: :signup, in: :body, schema:{
+        type: :object,
+        properties: {
+          name: { type: :string },
+          phone: { type: :string },
+          gender: { type: :string, enum: %w[male female other] },
+          email: { type: :string },
+          birthday: { type: :string },
+          password: { type: :string }
+        },
+        required: %w[id name gender email password]
+      }
+      response(201, 'created') do
+        consumes 'application/json'
+        schema type: :object,
+          properties: {
+            id: { type: :integer },
+            name: { type: :string },
+            phone: { type: :string },
+            gender: { type: :string, enum: %w[male female other] },
+            email: { type: :string },
+            birthday: { type: :string }
+          },
+          required: %w[id name gender email]
+        run_test!
+      end
+      response(400, 'bad request') do
+        example 'application/json', :example_already_exists, {
+          errors: [
+              "Email has already been taken",
+              "Name has already been taken"
+          ]
+        }
+        run_test!
+      end
+    end
+  end
+end
