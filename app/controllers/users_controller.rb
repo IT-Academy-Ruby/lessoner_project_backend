28dfda4e1e32e429cae2ad27ae742eefa27db6b4
Rita Miashchanskaya <110627917+ritamma@users.noreply.github.com>
2022-10-17 21:39:37 +0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
 
   def check_username
     @user = User.find_by(name: params[:name])
-
+  end
+  
   def check_email
     @user = User.find_by(email: params[:email])
   end
