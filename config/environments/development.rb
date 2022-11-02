e7df663c061f1a6f008edb9e2c8bc41fd14a1707
Ritamma <rita07041998@gmail.com>
2022-11-03 00:42:10 +0300

    Updated for rubocop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,9 +73,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  host = 'localhost:3000'
-
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
