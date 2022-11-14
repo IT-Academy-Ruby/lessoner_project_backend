7fd94d73f26dd7ea1eb6dc4315fda9f5ce2122a2
Ritamma <rita07041998@gmail.com>
2022-11-14 13:15:52 +0300

    Updated for rubocop

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -10,7 +10,8 @@ class Lesson < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 64, too_long: 'The maximum allowed number of character is 64' },
                     format: { with: %r/\A[а-яА-ЯёЁa-zA-Z0-9`:%&'"@,><{|}=_~\-!()$*+\/?\^.\[\]]+\z/ }
-  validates :description, presence: true, length: { maximum: 600, too_long: 'The maximum allowed number of character is 600' },
+  validates :description, presence: true,
+                          length: { maximum: 600, too_long: 'The maximum allowed number of character is 600' },
                           format: { with: %r/\A[а-яА-ЯёЁa-zA-Z0-9`%&'"@,><{|}=_~\-!()$*+\/?\^.\[\]]+\z/,
                                     message: 'This character is not available for input in this field' }
   validates :video_link, presence: true, url: { message: 'Please check the correctness of the link' }
