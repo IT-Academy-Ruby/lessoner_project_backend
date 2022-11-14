e9429786da25fcd5f9e90d62f7adaa3046814124
Ritamma <rita07041998@gmail.com>
2022-11-14 16:00:20 +0300

    Add constants

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,11 +8,14 @@ class Lesson < ApplicationRecord
   belongs_to :author, class_name: 'User'
   enum :status, STATUSES
 
-  validates :title, presence: true, length: { maximum: 64, too_long: 'The maximum allowed number of character is 64' },
-                    format: { with: %r/\A[а-яА-ЯёЁa-zA-Z0-9`:%&'"@,><{|}=_~\-!()$*+\/?\^.\[\]]+\z/ }
+  validates :title, presence: true,
+                    length: { maximum: TITLE_MAX_SIZE,
+                              too_long: "The maximum allowed number of character is #{TITLE_MAX_SIZE}" },
+                    format: { with: REG_EXP_FOR_LESSON }
   validates :description, presence: true,
-                          length: { maximum: 600, too_long: 'The maximum allowed number of character is 600' },
-                          format: { with: %r/\A[а-яА-ЯёЁa-zA-Z0-9`%&'"@,><{|}=_~\-!()$*+\/?\^.\[\]]+\z/,
+                          length: { maximum: DESCRIPTION_MAX_SIZE,
+                                    too_long: "The maximum allowed number of character is #{DESCRIPTION_MAX_SIZE}" },
+                          format: { with: REG_EXP_FOR_LESSON,
                                     message: 'This character is not available for input in this field' }
   validates :video_link, presence: true, url: { message: 'Please check the correctness of the link' }
   validates :category_id, presence: true
