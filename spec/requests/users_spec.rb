9b38ae359862e7409c55f699138db8a34b632d0a
Ritamma <rita07041998@gmail.com>
2022-11-24 15:10:58 +0300

    Add swagger

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -34,35 +34,5 @@ RSpec.describe 'users', type: :request do
       end
     end
   end
-  path '/check_email?email={email}' do
-    parameter name: 'email', in: :path, type: :string, description: 'email'
 
-    get('check email') do
-      produces 'application/json'
-      response(200, 'successful') do
-        schema type: :object,
-               properties: {
-                 email_exists: { type: :boolean, example: 'true' }
-               },
-               required: %w[email_exists]
-        run_test!
-      end
-    end
-  end
-  path '/check_username?name={username}' do
-    parameter name: 'username', in: :path, type: :string, description: 'username'
-
-    get('check username') do
-      tags 'Users'
-      produces 'application/json'
-      response(200, 'successful') do
-        schema type: :object,
-               properties: {
-                 username_exists: { type: :boolean, example: 'true' }
-               },
-               required: %w[username_exists]
-        run_test!
-      end
-    end
-  end
 end
