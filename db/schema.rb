5844d19657b1ea8abfd7e14704f1365c81a351e0
evrusha <uzaharau@gmail.com>
2022-11-29 18:46:01 +0300

    Added column admin_type to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_170422) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_154130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_11_170422) do
     t.datetime "password_reset_sent_at"
     t.boolean "email_confirmed", default: false
     t.string "confirm_token"
+    t.boolean "admin_type", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
   end
