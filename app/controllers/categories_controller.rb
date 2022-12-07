b5c8ac64af3aeef75536ea37187bca767d70eadd
Ritamma <rita07041998@gmail.com>
2022-12-07 20:05:46 +0300

    Delete method from categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -49,13 +49,6 @@ class CategoriesController < ApplicationController
     @category = Category.find_by(id: params[:id])
   end
 
-  def current_user_id
-    @decoded = JsonWebToken.decode(jwt_token)
-    @current_user = User.find_by(email: @decoded['email'])
-  rescue
-    nil
-  end
-
   def for_admin
     if jwt_token.present?
       render json: { error: "You don't have permission to access" }, status: :forbidden unless current_user.admin_type?
