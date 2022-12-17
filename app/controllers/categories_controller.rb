734181d9f1efcf59db6f5bfa088fa83235bd4356
evrusha <uzaharau@gmail.com>
2022-12-17 16:41:01 +0300

    Sfix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class CategoriesController < ApplicationController
   before_action :for_admin, only: %i[new create edit update]
   before_action :category_find, only: %i[show edit update]
