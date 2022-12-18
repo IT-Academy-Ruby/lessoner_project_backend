87175f7f825e5c1cf833428431db72496a5ea433
evrusha <uzaharau@gmail.com>
2022-12-18 12:35:22 +0300

    Added pagy to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,6 @@ class Comment < ApplicationRecord
   belongs_to :lesson
   belongs_to :user
   validates :body, presence: true
+  scope :filter_by_user_id, ->(user_id) { where user_id: }
+  scope :filter_by_lesson_id, ->(lesson_id) { where lesson_id: }
 end
