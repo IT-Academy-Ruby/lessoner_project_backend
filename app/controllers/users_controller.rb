f953b732fdbb50ba94f765ce6a12b56192d6ac79
evrusha <uzaharau@gmail.com>
2022-12-19 19:47:01 +0300

    Adde update email and password for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
-# frozen_string_literal: true
-
 class UsersController < AuthorizationController
+  before_action :update_password, only: :update
+  before_action :send_email, only: :update
+
   def show
     if current_user
       render :show
@@ -17,13 +18,7 @@ class UsersController < AuthorizationController
       current_user.avatar_url = current_user.avatar&.url&.split('?')&.first
       current_user.save!
     end
-    if params[:email].present?
-      current_user.generate_update_email_token!
-      new_email = params[:email]
-      UserMailer.update_email_information(current_user, new_email).deliver_now
-      UserMailer.update_email_confirmation(current_user, new_email).deliver_now
-      render json: { user: 'sent' }, status: :ok
-    elsif current_user.update(user_params)
+    if current_user.update(user_params)
       render :show
     else
       render :error, status: :unprocessable_entity
@@ -34,5 +29,28 @@ class UsersController < AuthorizationController
 
   def user_params
     params.permit(:name, :description, :avatar, :avatar_url, :gender, :birthday)
+  end
+
+  def update_password
+    @password = params[:password]
+    return if @password.blank?
+
+    if current_user.authenticate(params[:current_password])
+      return render :error unless current_user.update(password: @password)
+
+      render :show
+    else
+      render json: { error: 'current password does not match' }, status: :forbidden
+    end
+  end
+
+  def send_email
+    return if params[:email].blank?
+
+    current_user.generate_update_email_token!
+    new_email = params[:email]
+    UserMailer.update_email_information(current_user, new_email).deliver_now
+    UserMailer.update_email_confirmation(current_user, new_email).deliver_now
+    render json: { user: 'sent' }, status: :ok
   end
 end
