5b65deae5d264e5438e4c81319251dd6a9d4dde5
PandaGrom <arsenij.grom@mail.ru>
2023-01-12 23:34:05 +0300

    Change rubocops config and add relation in lessons model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,6 +8,7 @@ class Lesson < ApplicationRecord
   has_many :lesson_views, dependent: :destroy
   has_one_attached :lesson_image, dependent: :destroy
   has_one_attached :lesson_video, dependent: :destroy
+  belongs_to :category
   belongs_to :author, class_name: 'User'
   enum :status, STATUSES
 
